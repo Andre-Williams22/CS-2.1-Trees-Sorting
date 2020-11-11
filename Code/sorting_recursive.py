92453d02f2672855abf63e8f112d961b48aa87e7
andre <awjrs22@gmail.com>
2020-11-10 23:27:46 -0500

    improves sorting algorithm merge sort

--- Code/sorting_recursive.py
+++ Code/sorting_recursive.py
@@ -4,8 +4,12 @@
 def merge(items1, items2):
     """Merge given lists of items, each assumed to already be in sorted order,
     and return a new list containing all items in sorted order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
+    TODO: Running time: O(n) Why and under what conditions?
+    We'll be looping through the array and the length of the array is deterministic of the time. 
+    TODO: Memory usage: O(n) Why and under what conditions? 
+    We're creating a new array and will store our new items in the array. 
+    
+    """
     # TODO: Repeat until one list is empty
     # TODO: Find minimum item in both lists and append it to new list
     # TODO: Append remaining items in non-empty list to new list
@@ -38,21 +42,24 @@ def merge(items1, items2):
             # move forward in both lists
             p1 += 1 
             p2 += 1 
-
     # check if one array is done. Then add the entire other array to the end of the list 
     if len(items1) == p1:
         for item in range(p2, len(items2)):
             solution.append(items2[item])
+    
     if len(items2) == p2:
         for item in range(p2, len(items2)):
             solution.append(items2[item])
     
+    # return the new array 
     return solution
 
-    # s.extend(b[pointer:])
-    # items3.extend(items1[i:])
-    #     items3.extend(items2[j:])
+def iterative_merge_helper(left, right):
+    if not len(left) or not len(right):
+        return left or right 
 
+    results = []
+    i, j = 0, 0
 
 
 def split_sort_merge(items):
@@ -64,18 +71,52 @@ def split_sort_merge(items):
     # TODO: Split items list into approximately equal halves
     # TODO: Sort each half using any other sorting algorithm
     # TODO: Merge sorted halves into one list in sorted order
+    if len(items) <2:
+        return items
+    middle = len(items) // 2 
+    left = iterative_merge_helper(items[:middle])
+    right = iterative_merge_helper(items[middle:])
 
-
+    return iterative_merge_helper(left, right)
+    
 def merge_sort(items):
     """Sort given items by splitting list into two approximately equal halves,
     sorting each recursively, and merging results into a list in sorted order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
+    TODO: Running time: O(nlog(n)) Why and under what conditions? the array  
+    TODO: Memory usage: O(nlog(n)) Why and under what conditions?
+    
+    """
     # TODO: Check if list is so small it's already sorted (base case)
     # TODO: Split items list into approximately equal halves
     # TODO: Sort each half by recursively calling merge sort
     # TODO: Merge sorted halves into one list in sorted order
+    if len(items) == 1:
+        return items 
+    middleIdx = len(items) // 2 
+    leftHalf = items[:middleIdx]
+    rightHalf == items[middleIdx:]
+    return mergeSortedArrays(merge_sort(leftHalf), merge_sort(rightHalf))
 
+def mergeSortedArrays(leftHalf, rightHalf):
+    sortedArray = [None] * (len(leftHalf) + len(rightHalf))
+    k = i = j = 0
+    while i < len(leftHalf) and j < len(rightHalf):
+        if leftHalf[i] <= rightHalf[j]:
+            sortedArray[k] = leftHalf[i]
+            i += 1 
+        else:
+            sortedArray[k] = rightHalf[j]
+            j += 1 
+        k += 1 
+    while i < len(leftHalf):
+        sortedArray[k] = leftHalf[i]
+        i += 1 
+        k += 1 
+    while j < len(rightHalf):
+        sortedArray[k] = rightHalf[j]
+        j += 1 
+        k += 1 
+    return sortedArray
 
 def partition(items, low, high):
     """Return index `p` after in-place partitioning given items in range
