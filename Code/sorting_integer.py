b718d747a111beca19e84a9b52604b235a4aa8d7
Andre Williams <awjrs22@gmail.com>
2020-11-18 22:48:11 -0600

    finishes bucket sort and passes tests

--- Code/sorting_integer.py
+++ Code/sorting_integer.py
@@ -32,6 +32,55 @@ def counting_sort(numbers):
     # return sorted list 
     return sorted_list
 
+ 
+     
+def merge_sort(items):
+    """Sort given items by splitting list into two approximately equal halves,
+    sorting each recursively, and merging results into a list in sorted order.
+    TODO: Running time: O(nlog(n)) Why and under what conditions? the array is divided in two halves. the merge is a key process
+    that assumes that they're sorted and merges the two sorted sub-arrays into one.
+    TODO: Memory usage: O(nlog(n)) Why and under what conditions? I need to use recursion and am doing it in place.
+    There will be memory usage because of frames on the call stack. There will be log(n) calls on the array. 
+    
+    """
+    # TODO: Check if list is so small it's already sorted (base case)
+    # TODO: Split items list into approximately equal halves
+    # TODO: Sort each half by recursively calling merge sort
+    # TODO: Merge sorted halves into one list in sorted order
+    
+    # checks base case 
+    if len(items) == 1:
+        return items 
+        
+    middleIdx = len(items) // 2 
+    leftHalf = items[:middleIdx]
+    rightHalf = items[middleIdx:]
+    return recursiveMergeSortedArrays(merge_sort(leftHalf), merge_sort(rightHalf))
+
+def recursiveMergeSortedArrays(leftHalf, rightHalf):
+    sortedArray = [None] * (len(leftHalf) + len(rightHalf))
+    k = i = j = 0
+    while i < len(leftHalf) and j < len(rightHalf):
+        # if left half is smaller 
+        if leftHalf[i] <= rightHalf[j]:
+            sortedArray[k] = leftHalf[i]
+            i += 1 
+        else:
+            sortedArray[k] = rightHalf[j]
+            j += 1 
+        k += 1 
+    while i < len(leftHalf):
+        sortedArray[k] = leftHalf[i]
+        i += 1 
+        k += 1 
+    while j < len(rightHalf):
+        sortedArray[k] = rightHalf[j]
+        j += 1 
+        k += 1 
+    return sortedArray
+
+
+
 def grab_largest_nums(numbers):
     largest = 0
     for item in numbers:
@@ -40,7 +89,6 @@ def grab_largest_nums(numbers):
             largest = item
             
     return len(str(largest))
- 
 
 def bucket_sort(numbers, num_buckets=10):
     """Sort given numbers by distributing into buckets representing subranges,
@@ -48,9 +96,6 @@ def bucket_sort(numbers, num_buckets=10):
     TODO: Running time: O(nw) which is O(n) Why and under what conditions? N is the number of keys or digits 
     and w is the length of the longest key.
     TODO: Memory usage: ??? Why and under what conditions?
-    
-    
-    
     """
     # TODO: Find range of given numbers (minimum and maximum values)
     # TODO: Create list of buckets to store numbers in subranges of input range
@@ -59,6 +104,36 @@ def bucket_sort(numbers, num_buckets=10):
     # TODO: Loop over buckets and append each bucket's numbers into output list
     # FIXME: Improve this to mutate input instead of creating new output list
 
-    grab_largest_nums(numbers)
 
+   # grab_largest_nums(numbers)
+    
+    copy = []
+    minimum = numbers[0]
+    maximum = numbers[0]
+
+
+    for item in numbers:
+        if minimum > item:
+            minimum = 1
+        if maximum < item:
+            maximum = item
+
+    for i in range(0,len(numbers)):
+        copy.append([])
+
+    for item in numbers:
+        unique_index = int(item * len(numbers) / (maximum + 1))
+        copy[unique_index].append(item)
+
+    for i in range(len(copy)):
+        # call merge sort
+        new_list[i] = merge_sort(copy[i])
+
+    numbers.clear() #Empty the list
+
+    for i in range(len(copy)):
+        for item in copy[i]:
+            numbers.append(item)
+
+    return numbers
 
